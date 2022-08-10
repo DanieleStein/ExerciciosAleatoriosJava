f73d6e872f522b47a6aff213fa0d23ef3b8149f8
DanieleStein <danieleastein@gmail.com>
2022-08-10 10:02:56 -0300

    Criado o package dicionarios, e exerciciio ApelidosNomes e LoteriaSonhos.

---
+++ src/br/com/dicionarios/ApelidosNomeHashMap.java
@@ -0,0 +1,25 @@
+package br.com.dicionarios;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApelidosNomeHashMap {
+	
+	public static void main(String[] args) {
+		
+	
+	
+	Map<String, String> nomes = new HashMap<>();
+	
+	nomes.put("João", "Juan, Fissura, Maromba");
+	nomes.put("Miguel", "Night Watch, Bruce Wayne, Tampinha");
+	nomes.put("Maria", "Wonder Woman, Mary, Marilene");
+	nomes.put("Lucas", "Lukinha, Jorge, George");
+	
+	System.out.println(nomes);
+	
+	for (String nomeMesmo : nomes.keySet()) {
+		System.out.println("O nome " + nomeMesmo + ", tem os seguintes apelidos: " + nomes.get(nomeMesmo));
+	}
+	}
+}
